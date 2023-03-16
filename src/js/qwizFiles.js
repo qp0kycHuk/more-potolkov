011ae00fc7792466d0b532deefc34ec44416d7b8
qp0kycHuk <serega990112@mail.ru>
2023-03-16 12:45:26 +0300

    add submit function to quiz

--- src/js/qwizFiles.js
+++ src/js/qwizFiles.js
@@ -50,6 +50,42 @@ document.addEventListener('submit', (event) => {
     if (!event.target.classList.contains('quiz')) return
     event.preventDefault()
 
+    const formData = new FormData(event.target)
+    items.forEach((item) => {
+        formData.append('file', item.file)
+    })
+
+    const btn = event.target.querySelector('[type="submit"]')
+    const btnHtml = btn.innerHTML
+
+    btn.disabled = true
+    btn.style.width = btn.clientWidth + 'px'
+    btn.innerHTML = '<div class="text-small progress progress-circle fade-80"></div>'
+
+    fetch('/submitHandler.php', {
+        method: 'post',
+        body: formData
+    })
+        .then((response) => response.text())
+        .then((result) => {
+            btn.disabled = false
+            btn.innerHTML = btnHtml
+            btn.style.width = ''
+
+            const customEvent = new CustomEvent('successsubmit', {
+                bubbles: true,
+                cancelable: true,
+            });
+
+            event.target.dispatchEvent(customEvent);
+
+            window.Fancybox.close()
+            window.Fancybox.modal.open('/dialog-success.php', { type: 'ajax' })
+            event.target.reset()
+
+
+            // ym(80446801, 'reachGoal', 'zayavka'); return true;
+        })
 
 })
 
